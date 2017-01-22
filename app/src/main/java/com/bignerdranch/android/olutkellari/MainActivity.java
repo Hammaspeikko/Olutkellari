c1372971316515e9e1e024f68488b6b92c4c565d
Hammaspeikko <sami-holopainen@hotmail.com>
2017-01-22 18:34:16 +0200

    Added a onclicklisteners and created the base of the logic

--- app/src/main/java/com/bignerdranch/android/olutkellari/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/olutkellari/MainActivity.java
@@ -2,6 +2,8 @@ package com.bignerdranch.android.olutkellari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button selaaOluita = (Button) findViewById(R.id.selaaOluita);
+        Button arvosteleOlut = (Button) findViewById(R.id.arvosteleOlut);
+        Button profiili = (Button) findViewById(R.id.profiili);
+
+        selaaOluita.setOnClickListener((View.OnClickListener) this);
+        arvosteleOlut.setOnClickListener((View.OnClickListener) this);
+        profiili.setOnClickListener((View.OnClickListener) this);
 
     }
+
+        void onClick(View v){
+
+            final int selaa = R.id.selaaOluita;
+            final int arvostele = R.id.arvosteleOlut;
+            final int profiili = R.id.profiili;
+            int id = v.getId();
+            if(id == selaa){
+
+            }else if(id == arvostele){
+
+            }else if(id == profiili){
+
+            }
+
+        }
 }
