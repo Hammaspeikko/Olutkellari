8f7011ab4f24d73b0e1f8979a552ae97eebbe8ac
Hammaspeikko <sami-holopainen@hotmail.com>
2017-01-23 19:07:49 +0200

    Added profiili, arvostele and selaa activities
    also made a connection between main and them

---
+++ app/src/main/java/com/bignerdranch/android/olutkellari/ArvosteleOlutActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.olutkellari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArvosteleOlutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arvostele_olut);
+    }
+}
--- app/src/main/java/com/bignerdranch/android/olutkellari/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/olutkellari/MainActivity.java
@@ -1,11 +1,12 @@
 package com.bignerdranch.android.olutkellari;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,23 +17,27 @@ public class MainActivity extends AppCompatActivity {
         Button arvosteleOlut = (Button) findViewById(R.id.arvosteleOlut);
         Button profiili = (Button) findViewById(R.id.profiili);
 
-        selaaOluita.setOnClickListener((View.OnClickListener) this);
-        arvosteleOlut.setOnClickListener((View.OnClickListener) this);
-        profiili.setOnClickListener((View.OnClickListener) this);
+        selaaOluita.setOnClickListener(this);
+        arvosteleOlut.setOnClickListener(this);
+        profiili.setOnClickListener(this);
 
     }
 
-        void onClick(View v){
+        public void onClick(View v){
 
             final int selaa = R.id.selaaOluita;
             final int arvostele = R.id.arvosteleOlut;
             final int profiili = R.id.profiili;
             int id = v.getId();
             if(id == selaa){
-
+                Intent intent = new Intent(getApplicationContext(), SelaaActivity.class);
+                startActivity(intent);
             }else if(id == arvostele){
-
+                Intent intent = new Intent(getApplicationContext(), ArvosteleOlutActivity.class);
+                startActivity(intent);
             }else if(id == profiili){
+                Intent intent = new Intent(getApplicationContext(), ProfiiliActivity.class);
+                startActivity(intent);
 
             }
 
