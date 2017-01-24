50c325c8d0bc15ccfa2a9dd930f2a952bd0c4232
Hammaspeikko <sami-holopainen@hotmail.com>
2017-01-24 11:34:10 +0200

    Created a UI for beer ratings
    Also added foundation logic for fetching the countries from the device

--- app/src/main/java/com/bignerdranch/android/olutkellari/ArvosteleOlutActivity.java
+++ app/src/main/java/com/bignerdranch/android/olutkellari/ArvosteleOlutActivity.java
@@ -2,6 +2,12 @@ package com.bignerdranch.android.olutkellari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 public class ArvosteleOlutActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class ArvosteleOlutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arvostele_olut);
+
+        //Spinner maat = (Spinner) findViewById();
+
+    }
+
+    private List<String> haeMaat (){
+        //Hakee kaikki maat
+        Locale[] locales = Locale.getAvailableLocales();
+        ArrayList<String> maat = new ArrayList<String>();
+
+        //Lisätään kaikki maat listaan (poislukien tyhjät ja tuplat)
+        for(Locale l : locales){
+            String maa = l.getDisplayCountry().trim();
+            if(!maat.contains(maa) && maa.length() > 0){
+                maat.add(maa);
+            }
+        }
+
+        // Maat aakkosjärjestykse
+        Collections.sort(maat);
+        return maat;
     }
 }
