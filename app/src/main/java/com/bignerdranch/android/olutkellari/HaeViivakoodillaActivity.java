65564f0a9ca8e20fdc01ca0557c5e8c9a4334d32
Hammaspeikko <sami-holopainen@hotmail.com>
2017-02-21 11:08:35 +0200

    Fixed a permission bug

--- app/src/main/java/com/bignerdranch/android/olutkellari/HaeViivakoodillaActivity.java
+++ app/src/main/java/com/bignerdranch/android/olutkellari/HaeViivakoodillaActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.olutkellari;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,7 +16,11 @@ public class HaeViivakoodillaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, SkannaaActivity.class);
-        startActivityForResult(intent,0);
+        if (ActivityCompat.checkSelfPermission(HaeViivakoodillaActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(HaeViivakoodillaActivity.this, new String[]{android.Manifest.permission.CAMERA}, 1);
+        }else{
+            startActivityForResult(intent,0);
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -41,6 +47,29 @@ public class HaeViivakoodillaActivity extends AppCompatActivity {
 
         }else{
             super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, SkannaaActivity.class);
+                    startActivityForResult(intent,0);
+                } else {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(this,"Viivakoodinluku tarvitsee oikeudet kameraan!",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 }
