5c33d9816720180b1b0ed7a4da570ae07c2b5a23
Hammaspeikko <sami-holopainen@hotmail.com>
2017-02-23 18:41:38 +0200

    Modified the beer type list

--- app/src/main/java/com/bignerdranch/android/olutkellari/ArvosteleOlutActivity.java
+++ app/src/main/java/com/bignerdranch/android/olutkellari/ArvosteleOlutActivity.java
@@ -79,20 +79,13 @@ public class ArvosteleOlutActivity extends AppCompatActivity implements View.OnC
 
     private List<String> haeOlutTyypit(){
         List<String> tyypit = new ArrayList<String>();
-        tyypit.add("Altbier"); tyypit.add("Amber ale"); tyypit.add("Barley wine"); tyypit.add("Berliner Weisse");
-        tyypit.add("Bière de Garde"); tyypit.add("Bitter"); tyypit.add("Blonde Ale"); tyypit.add("Bock");
-        tyypit.add("Brown ale"); tyypit.add("California Common/Steam Beer");  tyypit.add("Cream Ale");
-        tyypit.add("Dortmunder Export"); tyypit.add("Doppelbock"); tyypit.add("Dunkel");
-        tyypit.add("Dunkelweizen"); tyypit.add("Eisbock"); tyypit.add("Flanders red ale");
-        tyypit.add("Golden/Summer ale"); tyypit.add("Gose"); tyypit.add("Gueuze");
-        tyypit.add("Hefeweizen");  tyypit.add("Helles"); tyypit.add("India pale ale");
-        tyypit.add("Kölsch"); tyypit.add("Lambic"); tyypit.add("Light ale");
-        tyypit.add("Maibock/Helles bock"); tyypit.add("Malt liquor");
-        tyypit.add("Mild"); tyypit.add("Oktoberfestbier/Märzenbier"); tyypit.add("Old ale");
-        tyypit.add("Oud bruin"); tyypit.add("Pale ale"); tyypit.add("Pilsener/Pilsner/Pils");
-        tyypit.add("Porter"); tyypit.add("Red ale"); tyypit.add("Roggenbier"); tyypit.add("Saison");
-        tyypit.add("Scotch ale"); tyypit.add("Stout"); tyypit.add("Schwarzbier"); tyypit.add("Vienna lager");
-        tyypit.add("Witbier"); tyypit.add("Weissbier"); tyypit.add("Weizenbock");
+        tyypit.add("Vehnäolut"); tyypit.add("Berliner Weisse"); tyypit.add("Lambic"); tyypit.add("Stout");
+        tyypit.add("Portteri"); tyypit.add("Ale"); tyypit.add("Bitter"); tyypit.add("Trappist");
+        tyypit.add("Alt"); tyypit.add("Kölsch");  tyypit.add("Sahti");
+        tyypit.add("Lager"); tyypit.add("Light"); tyypit.add("Pils");
+        tyypit.add("Rauchbier"); tyypit.add("Dortmunder"); tyypit.add("Wieniläinen, wiener");
+        tyypit.add("Märzen"); tyypit.add("Münchener-olut"); tyypit.add("Bock");
+
 
         Collections.sort(tyypit);
         return tyypit;
